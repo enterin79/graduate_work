824a6d80c4c6a37fc38c6b82bb66c1a885fe5c45
enterin79 <nastuazh@gmail.com>
2021-02-15 04:02:38 +0300

    добавлено описание БД

--- testpr1/main.cpp
+++ testpr1/main.cpp
@@ -1,3 +1,38 @@
+/*Создание бд (без особых проверок, чисто для тестирования приложения, но так должны быть все name уникальными, а даты проверяться)
+ * create table Equipment(
+    id serial primary key,
+    NameEquip VARCHAR(40) not null,
+    FirstExplDate date not null,
+    Note varchar(300)
+);
+create table Test(
+    id serial primary key,
+    NameTest VARCHAR(40) not null,
+    DateTest date not null,
+    Description varchar(300),
+    idEqip int not null references Equpment(id)
+);
+create table Synonim(
+    idtable serial primary key,
+    nametable varchar(100) not null,
+    synonim varchar(100) not null
+);
+insert into Equpment(NameEquip, FirstExplDate) values
+('оборудование 1', '2021-01-01'),
+('оборудование 2', '2021-01-02'),
+('оборудование 3', '2021-01-03');
+
+insert into test(NameTest, DateTest, idEqip) values
+('проверка оборудования 1', '2021-01-02', 1),
+('проверка оборудования 2', '2021-01-03', 2),
+('проверка оборудования 3', '2021-01-04', 3),
+('тестовая загрузка оборудования 1', '2021-01-05', 1),
+('тестовая загрузка оборудования 2', '2021-01-06', 2);
+
+insert into Synonim(nametable, synonim) values
+('test', 'Тесты'),
+('equipment', 'Оборудование');
+*/
 #include "mainwindow.h"
 
 #include <QApplication>
@@ -37,6 +72,8 @@ void logHandler(QtMsgType type, const QMessageLogContext &context, const QString
         case QtInfoMsg:     out << "INF "; break;   //Запись необходимой аббревиатуры
         case QtWarningMsg:  out << "WRN "; break;
         case QtCriticalMsg: out << "CRT "; break;
+        case QtFatalMsg:    out << "FTL "; break;
+        case QtDebugMsg:    out << "DBG "; break;
     }
     out << context.category << ": "<< msg << endl;  //Запись информации по указанной категории логирования
     out.flush();    //Очистка буфера записи
