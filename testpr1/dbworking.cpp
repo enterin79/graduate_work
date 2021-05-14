52064d067b065e4578db8ed0b355093969624fc3
enterin79 <nastuazh@gmail.com>
2021-05-14 05:33:17 +0300

    -

--- testpr1/dbworking.cpp
+++ testpr1/dbworking.cpp
@@ -29,7 +29,7 @@ Enumerr dbWorking::connection()
      db.setHostName("127.0.0.1");
      db.setDatabaseName("testing");
      db.setUserName("postgres");
-     db.setPassword("admin123");
+     db.setPassword("");
      if(db.open()) {//Выполнение подключения к базе данных с текущими реквизитами
          qInfo(loggerInfo())<<"Connection OK";
          choosingmodel=new QSqlQueryModel;   //Загрузка списка доступных таблиц из таблицы мета-данных
